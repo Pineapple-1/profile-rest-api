32de78954822c3dcddd7fe402e453b29fa9e0268
abdulrehman ajmal <abdulrehman.ajmal@outlook.com>
2021-08-09 02:46:19 +0500

    added profile feed

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,7 @@ from profiles_api import serializers, models, permissions
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
@@ -25,6 +26,9 @@ class ProfileFeedItemViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.ProfileFeedItemSerializer
     queryset = models.ProfileFeedItem.objects.all()
     authentication_classes = (TokenAuthentication,)
-    filter_backends = (filters.SearchFilter,)
-    search_fields = ['user_profile']
+    permission_classes = (permissions.UpdateOwnStatus,IsAuthenticated)
+    
+
+    def perform_create(self, serializer):
+        serializer.save(user_profile= self.request.user)
     
