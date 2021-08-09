48db55e6e200d2a60fd287a6b47ea35f94de1906
abdulrehman ajmal <abdulrehman.ajmal@outlook.com>
2021-08-10 00:43:51 +0500

    refactor

---
+++ Script.py
@@ -0,0 +1,28 @@
+import requests
+
+
+TOKEN_ENDPOINT = 'https://mydjangoapiapp.herokuapp.com/login/'
+PROFILE_ENDPOINT = 'https://mydjangoapiapp.herokuapp.com/profile/'
+
+def get_token(email, password):
+    """Retrieve the token for logging in user"""
+    data = {'username': email, 'password': password}
+    res = requests.post(TOKEN_ENDPOINT, data=data)
+
+    return res.json()['token']
+
+
+def get_profile(profile_id, token):
+    """Retrive the profile for the logged in user"""
+    url = '{}{}/'.format(PROFILE_ENDPOINT, profile_id)
+    res = requests.get(url, headers={
+        'Content-Type': 'application/json',
+        'Authorization': 'Token {}'.format(token)
+    })
+
+    return res.json()
+
+token = get_token('abdulrehman.ajmal@outlook.com', 'oreo@123')
+profile = get_profile(1, token)
+print(profile)
+
